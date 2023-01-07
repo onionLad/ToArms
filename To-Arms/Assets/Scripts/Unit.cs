8f6c1799ca776c2bc6b77d09f14fbed1fbbed618
Bill <bill.xia.pa@gmail.com>
2023-01-07 13:54:14 -0500

    Unit Class
    Created a unit class, which handles unit stats.

---
+++ To-Arms/Assets/Scripts/Unit.cs
@@ -0,0 +1,84 @@
+/*
+ * Unit.cs
+ *
+ * Purpose: Contains the Unit class, with contains all information about a
+ *          given unit, including its health, combat modifiers, and stats.
+ */
+
+/* ======================================================================== *\
+ *  IMPORTS                                                                 *
+\* ======================================================================== */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* ======================================================================== *\
+ * CLASS: Unit                                                              *
+ * Description: An instance of this class is needed for each unit in each   *
+ *              battle scene. It gets attached to a unit GameObject.        *
+\* ======================================================================== */
+public class Unit : MonoBehaviour
+{
+    /* ==================================================================== *\
+     *  MEMBER VARIABLES                                                    *
+    \* ==================================================================== */
+
+    /*
+     * Unit Type Info.
+     * UnitName determines the base stats while modifer alters the base stats
+     * to adjust difficulty.
+     */
+    public string unitName;
+    public float modifier;
+
+    /* Unit Stats. */
+    private int maxHealth;
+    private int currHealth;
+    private int moveRange;
+
+    private int strength;
+    private int armor;
+    private int baseDamage;
+
+    private string weapon;
+
+    /* ==================================================================== *\
+     *                                                                      *
+     *  CLASS METHODS                                                       *
+     *                                                                      *
+    \* ==================================================================== */
+
+    /* On start, initialize member variables. */
+    void Start()
+    {
+        switch (unitName)
+        {
+            default:
+                maxHealth = 10;
+                currHealth = maxHealth;
+                moveRange = 2;
+
+                strength = 5;
+                armor = 5;
+                baseDamage = 5;
+
+                weapon = "None";
+                break;
+        }
+    }
+
+    /* Debugging function. Spits out unit stats to console. */
+    void DisplayStats()
+    {
+        Debug.Log
+        (
+            "Name: " + unitName + "\n" +
+            "Health: " + currHealth + "/" + maxHealth + "\n" +
+            "Move: " + moveRange + "\n" +
+            "Strength: " + strength + "\n" +
+            "Armor: " + armor + "\n" +
+            "Damage: " + baseDamage + "\n" +
+            "Weapon: " + weapon
+        );
+    }
+}
